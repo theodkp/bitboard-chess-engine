30a6ddee23ac933bb0cdcca2e78af69436084760
theo <theo.paraskeva123@gmail.com>
2024-12-06 14:00:07 +0000

    init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+
+using U64 = unsigned long long;
+
+
+///// Bit MANIPULATIONS
+
+//// MAIN 
+
+int main(){
+
+    std::cout << "chess";
+    std::cin.get();
+
+    return 0;
+}
