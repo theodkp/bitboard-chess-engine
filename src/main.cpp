91e501b49a7c4b275a438f08ec61ab87bc6da191
theo <theo.paraskeva123@gmail.com>
2024-12-10 21:18:07 +0000

    string coordinates and get lsf bit index function

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,18 @@ enum {
 
 enum {white,black};
 
+
+const char *square_to_coordinates[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+};
+
 // non-zero binary string if bit set otherwise zero
 inline bool get_bit(U64 bitboard, int square) {
     return bitboard & (1ULL << square);
@@ -44,15 +56,18 @@ inline void unset_bit(U64& bitboard, int square){
 // Count bits
 static inline int count_bits(U64 bitboard){
 
-    // int count = 0;
-
-    // while(bitboard){
-    //     bitboard&= bitboard-1;
-    //     count++;
-    // };
-
-    // return count;
     return __builtin_popcountll(bitboard);
+}
+
+
+static inline int get_lsf_bit_index(U64 bitboard){
+    if (bitboard){
+        return count_bits((bitboard & -bitboard)-1);
+    }
+    else{
+        return -1;
+    }
+    
 }
 
 /// Print bitboard visulisation
@@ -324,7 +339,7 @@ int main(){
 
     
 
-    std::cout << count_bits(block);
+    std::cout << square_to_coordinates[get_lsf_bit_index(block)];
     
    
     return 0;
