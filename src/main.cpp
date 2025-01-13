bdd2dc438c342ee7cd30e858d4e19bac1ebc758a
theo <theo.paraskeva123@gmail.com>
2025-01-13 01:31:57 +0000

    init piece square tables

--- src/main.cpp
+++ src/main.cpp
@@ -1958,6 +1958,80 @@ int material_score[12] = {
  -10000,      // black king 
 };
 
+// pawn positional score
+const int pawn_score[64] = {
+    90,  90,  90,  90,  90,  90,  90,  90,
+    30,  30,  30,  40,  40,  30,  30,  30,
+    20,  20,  20,  30,  30,  30,  20,  20,
+    10,  10,  10,  20,  20,  10,  10,  10,
+     5,   5,  10,  20,  20,   5,   5,   5,
+     0,   0,   0,   5,   5,   0,   0,   0,
+     0,   0,   0, -10, -10,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0
+};
+
+// knight positional score
+const int knight_score[64] = {
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5,   0,   0,  10,  10,   0,   0,  -5,
+    -5,   5,  20,  20,  20,  20,   5,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,   5,  20,  10,  10,  20,   5,  -5,
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5, -10,   0,   0,   0,   0, -10,  -5
+};
+
+// bishop positional score
+const int bishop_score[64] = {
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,  10,  10,   0,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,  10,   0,   0,   0,   0,  10,   0,
+     0,  30,   0,   0,   0,   0,  30,   0,
+     0,   0, -10,   0,   0, -10,   0,   0
+
+};
+
+// rook positional score
+const int rook_score[64] = {
+    50,  50,  50,  50,  50,  50,  50,  50,
+    50,  50,  50,  50,  50,  50,  50,  50,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,   0,  20,  20,   0,   0,   0
+
+};
+
+// king positional score
+const int king_score[64] = {
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   5,   5,   5,   5,   0,   0,
+     0,   5,   5,  10,  10,   5,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   0,   5,  10,  10,   5,   0,   0,
+     0,   5,   5,  -5,  -5,   0,   5,   0,
+     0,   0,   5,   0, -15,   0,  10,   0
+};
+
+// mirror positional score tables for opposite side
+const int mirror_score[128] = {
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2,
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4,
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8
+};
+
 // cumulative score of current board state, 0 would be even, we switch the sign by returning double negative for black
 int evaluate(){
     int score = 0;
@@ -1980,6 +2054,21 @@ int evaluate(){
             square = get_lsf_bit_index(bitboard);
             // add to our cumulative score
             score+= material_score[piece];
+
+
+            switch (piece){
+                case P: score+= pawn_score[square]; break;
+                case N: score+= knight_score[square]; break;
+                case R: score+= rook_score[square]; break;
+                case B: score+= bishop_score[square]; break;
+                case K: score+= king_score[square]; break;
+
+                case p: score-= pawn_score[mirror_score[square]]; break;
+                case n: score-= knight_score[mirror_score[square]]; break;
+                case r: score-= rook_score[mirror_score[square]]; break;
+                case b: score-= bishop_score[mirror_score[square]]; break;
+                case k: score-= king_score[mirror_score[square]]; break;
+            }
 
             // unset bit on current piece and move to next if available
             unset_bit(bitboard,square);
@@ -2223,7 +2312,7 @@ int main(){
 
 
     if (debug){
-        parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
+        parse_fen("rnbqkbnr/pppppppp/8/8/4P3//PPPP1PPP/RNBQKBNR w KQkq - 0 1 ");
         print_board();
         std::cout<< evaluate();
     }
